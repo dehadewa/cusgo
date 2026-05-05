306dd3abd78e75133157c103c2e1add4ae150ced
FirmanSyah2078 <vellex1307@gmail.com>
2026-05-05 20:53:59 +0700

    up

---
+++ src/dummyData.js
@@ -0,0 +1,42 @@
+export const teamData = [
+  {
+    nama: "Daviq Cahyo L.",
+    nim: "24104410043",
+    ttl: "Sidoarjo, 08 Maret 2006",
+    alamat: "Centong, Sanankulon, Kab. Blitar",
+    quote: '"Jarang terlihat namun sering terlibat"',
+    foto: "./foto/daviq.png"
+  },
+  {
+    nama: "Anisaturrohmah",
+    nim: "24104410044",
+    ttl: "Blitar, 20 Januari 2006",
+    alamat: "Dimoro, Sukorejo, Kota Blitar",
+    quote: '"Sembunyikan prosesmu, tunjukkan hasilmu"',
+    foto: "./foto/anisa.png"
+  },
+  {
+    nama: "Premas Aji Susilo",
+    nim: "24104410055",
+    ttl: "Blitar, 21 Desember 2004",
+    alamat: "Ringinanom, Udanawu, Kab. Blitar",
+    quote: '"Find beauty in simplicity."',
+    foto: "./foto/premas.png"
+  },
+  {
+    nama: "Firlian Firman Syah",
+    nim: "24104410057",
+    ttl: "Kediri, 5 Juli 2005",
+    alamat: "Pakunden, Sukorejo, Kota Blitar",
+    quote: '"Jangan berhenti bernafas"',
+    foto: "./foto/firman.png"
+  },
+  {
+    nama: "Putri Perwitasari",
+    nim: "24104410080",
+    ttl: "Blitar, 4 Maret 2006",
+    alamat: "Modangan, Nglegok, Kab. Blitar",
+    quote: '"Bergerak untuk bertumbuh, bertumbuh untuk berdampak"',
+    foto: "./foto/putri.png"
+  }
+];
